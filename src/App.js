f0d928bad6bc45257454051b5c497c55ef7dc530
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-09-19 23:26:53 +0700

    Practicing this.setState({})

--- src/App.js
+++ src/App.js
@@ -2,31 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
-  }
-]
-
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  state = {
+    greeting: 'Hello!',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
+      },
+      { 
+        title: "Full Metal Jacket",
+        poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
+      }
+    ]
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "Trainspotting",
+            poster: "https://resizing.flixster.com/OUEArjor-MbyCV6GqLU85Hk9jQI=/300x300/v1.bjs1NTQ3OTM7ajsxNzQ2NjsxMjAwOzIwMDA7MTMzMQ"
+          },
+          ...this.state.movies,
+        ]
+      })
+    }, 5000)
+  }
+
+  
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
-          return <Movie title={movie.title} poster={movie.poster} />
+        {this.state.movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component{
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }
+
+
     render(){
         return(
             <div>
@@ -13,6 +21,11 @@ class Movie extends Component{
 }
 
 class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
         return(
             <img src={this.props.poster} alt="Movie Poster" />
