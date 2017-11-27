33f4c4029c714a37c1226ffd4298283191eb87a5
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-27 14:23:58 +0700

    Updated URL

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {}
+  state = {};
 
-  componentDidMount(){
+  componentDidMount() {
     this._getMovies();
   }
 
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie 
-        title={movie.title_english}
-        poster={movie.large_cover_image} 
-        key={movie.id}
-        genres={movie.genres}
-        synopsis={movie.synopsis}
-      />
-    })
-    return movies
-  }
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.large_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
 
-  _getMovies =  async () => {
-    const movies = await this._callApi()
+  _getMovies = async () => {
+    const movies = await this._callApi();
     this.setState({
       movies
-    })
-  }
+    });
+  };
 
   _callApi = () => {
-    return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=download_count', {
-      mode: 'cors'
-    })
-    .then(potato => potato.json())
-    .then(json => json.data.movies)
-    .catch(err => console.log(err))
-  }
-  
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
+
   render() {
-    const { movies } = this.state;
+    const { movies } = this.state;
     return (
       <div className={movies ? "App" : "App--loading"}>
-        {movies ? this._renderMovies() : 'Loading'}
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
 }
 
-export default App
+export default App;
